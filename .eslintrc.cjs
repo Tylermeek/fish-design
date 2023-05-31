97ab07adeb666e558c8ea3ff39c48c6ba32c316b
lijunjie01 <1755276345@qq.com>
2023-05-31 19:12:02 +0800

    wip: 暂存

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  emv: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true
+  },
+  globals: {
+    ga: true,
+    chromw: true,
+    __DEV__: true
+  },
+  // 支持解析.vue文件
+  parser: 'vue-eslint-parser',
+  extends:[
+    'plugin:json/recommended',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/prettier'
+  ],
+  plugins:['@typescript-eslint'],
+  parserOptions:{
+    parser: '@typescript-eslint/parser' // 支持解析ts文件
+  },
+  rules:{
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
