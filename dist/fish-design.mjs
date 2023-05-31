7cf7df4363ea56b71e1b147ab421e3a3937eee6a
TylerMeek <1755276345@qq.com>
2023-05-31 23:07:48 +0800

    feat: 打包构建配置优化

--- dist/fish-design.mjs
+++ dist/fish-design.mjs
@@ -1,43 +1,99 @@
 import { defineComponent, createVNode } from "vue";
 const __uno = "";
+const props$1 = {
+  icon: String
+};
+const Icon = defineComponent({
+  name: "Icon",
+  props: props$1,
+  setup(props2, {
+    slots
+  }) {
+    return () => createVNode("i", {
+      "class": `i-ic-baseline-${props2.icon} p-3`
+    }, null);
+  }
+});
 const props = {
   color: {
     type: String,
     default: "blue"
   },
+  plain: {
+    type: Boolean,
+    default: false
+  },
+  round: {
+    type: Boolean,
+    default: false
+  },
+  circle: {
+    type: Boolean,
+    default: false
+  },
+  size: {
+    type: String,
+    default: "default"
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
   icon: String
 };
-const FButton = defineComponent({
-  name: "FButton",
+const size = (size2) => {
+  switch (size2) {
+    case "small":
+      return "py-1 px-2";
+    case "large":
+      return "py-3 px-4";
+    default:
+      return "py-2 px-3";
+  }
+};
+const bgColor = ({
+  color,
+  plain,
+  disabled
+}) => `bg-${color}-${plain ? 100 : disabled ? 300 : 500}`;
+const round = (round2) => `rounded-${round2 ? "full" : "lg"}`;
+const hoverBg = ({
+  color,
+  plain
+}) => `hover:bg-${color}-${plain ? 500 : 700}`;
+const cursor = (disabled) => `cursor-${disabled ? "not-allowed" : "pointer"}`;
+const Button = defineComponent({
+  name: "Button",
   props,
-  setup(props, {
+  setup(props2, {
     slots
   }) {
     return () => createVNode("button", {
-      "class": `
-    py-2 
-    px-4 
+      "class": `    
+    ${size(props2.size)} 
+    ${bgColor(props2)}
+    ${hoverBg(props2)}
+    ${round(props2.round)}
+    ${cursor(props2.disabled)}
     font-semibold 
-    rounded-lg 
     shadow-md 
-    text-white 
-    bg-${props.color}-500 
-    hover:bg-${props.color}-700 
-    text-${props.plain ? props.color : white}
-    border-none 
-    cursor-pointer 
-    `
-    }, [props.icon !== "" ? createVNode("i", {
-      "class": `i-ic-baseline-${props.icon} p-3`
-    }, null) : "", slots.default ? slots.default() : ""]);
+    text-${props2.plain ? props2.color + "-500" : "white"}
+    hover:text-white
+    border-none
+    `,
+      "disabled": props2.disabled
+    }, [props2.icon && createVNode(Icon, {
+      "icon": props2.icon
+    }, null), slots.default ? slots.default() : ""]);
   }
 });
 const entry = {
   install(app) {
-    app.component(FButton.name, FButton);
+    app.component(Button.name, Button);
   }
 };
 export {
-  FButton,
+  Button,
   entry as default
 };
+//# sourceMappingURL=fish-design.mjs.map
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vite";
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import Unocss from './config/unocss'
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import Unocss from "./config/unocss";
 const rollupOptions = {
-
   external: ["vue", "vue-router"],
   output: {
     globals: {
@@ -15,37 +14,39 @@ const rollupOptions = {
 };
 
 export default defineConfig({
-  plugins:[
+  plugins: [
     vue(),
     // 添加JSX插件
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
     }),
-    Unocss()
+    Unocss(),
   ],
 
   // 添加库模式配置
   build: {
     rollupOptions,
-    minify:false,
-    cssCodeSplit:true,
+    minify: "terser",
+    sourcemap: true, // 输出单独的source文件以便debug
+    reportCompressedSize: true, // 生成压缩大小报告
+    cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
       name: "FishDesign",
       fileName: "fish-design",
       // 导出模块格式
-      formats: ["es", "umd","iife"],
+      formats: ["es", "umd", "iife"],
     },
   },
   // 单元测试配置
-  test:{
+  test: {
     // enable jest-like global test APIs
     globals: true,
     // 启用模拟dom，需要安装前置happy-dom
-    environment:'happy-dom',
+    environment: "happy-dom",
     // 支持tsx、jsx文件
-    transformMode:{
-      web:[/.[jt]sx$/]
-    }
-  }
-})
+    transformMode: {
+      web: [/.[jt]sx$/],
+    },
+  },
+});
