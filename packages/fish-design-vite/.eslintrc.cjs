52aef4968d0bf5e6360c65b520a2927c1f3dc03f
TylerMeek <1755276345@qq.com>
2023-06-11 17:02:33 +0800

    chore: lint文件修改

--- packages/fish-design-vite/.eslintrc.cjs
+++ packages/fish-design-vite/.eslintrc.cjs
@@ -27,5 +27,11 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "prettier/prettier": "error",
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ["Icon"],
+      },
+    ],
   },
 };
